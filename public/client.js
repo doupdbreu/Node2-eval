61567f2b6ed3e9c02771d07e2fe05069b5f87465
Pierre-Edouard <pe.debreuvand@gmail.com>
2018-11-15 17:18:55 +0100

    final vue

--- public/client.js
+++ public/client.js
@@ -1,1 +1,20 @@
-
+const vm = new Vue({
+    el : '#app',
+    template: `
+        <div class="template">
+            <h1>{{ title }}</h1>
+            <form v-on:submit.prevent>
+                <input class="champs" placeholder="Entrez votre texte" type="text"/>
+                <button class="btn-add" type="submit">valider</button>
+            </form>
+        </div>
+    `,
+    data: {
+        title: 'EVAL - Node2',
+        loading: false,
+        items: [],
+        formItem: {
+            text: '',
+        }
+    }
+})
