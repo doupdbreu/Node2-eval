447a895ab8528e6877e3135ac584f2d2e20577f0
Pierre-Edouard <pe.debreuvand@gmail.com>
2018-11-15 16:24:21 +0100

    static fonctionnal

--- app-server.js
+++ app-server.js
@@ -1,7 +1,8 @@
 const express = require('express')
-const port = 8000
-const app = express()
 
+const port = 8000
+
+const app = express()
 
 app.use(express.static('public'))
 
@@ -9,11 +10,8 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html')
 })
 
-app.use(
-    '/public', 
-    express.static(__dirname + '/public')
-)
-
+app.use('/public', express.static(__dirname + '/public'))
+  
 app.listen(port, () => {
     console.log(`Listening on ${port}`)
 })
--- public/client.js
+++ public/client.js
@@ -1,12 +1,1 @@
-Vue.component('eval', {
-    props: ['item'],
-    template: `
-        <div>
-            <h3>{{item.formattedDate}}</h3>
-        </div>
-    `
-})
 
-const vm = new Vue({
-   el: '#app' 
-})
